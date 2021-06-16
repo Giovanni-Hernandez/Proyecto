63068e34822307611b7acd391dab1720996aeeee
Jesús Morales <capueagles@gmail.com>
2021-06-16 15:36:39 -0500

    Validacion extra

--- js/validarCampo.js
+++ js/validarCampo.js
@@ -1,9 +1,23 @@
 function valCampo() {
-    var txt = document.getElementById("txtnumero").value;
+    var boleta = document.getElementById("txtnumero").value;
 
-    if (txt == null || txt.length == 0 || /^\s+$/.test(txt)) {
+    if (boleta == null || boleta.length == 0 || /^\s+$/.test(boleta)) {
         alert("Introduzca un número de boleta valido");
         return false;
+    }
+
+    for (var i = 0; i < boleta.length; i++) {
+        if (boleta[i] == "P" || boleta[i] == "E") {
+            var bandera = 1;
+            break;
+        }
+    }
+
+    if (bandera == 1) {
+        if (boleta[0] != "P" && boleta[1] != "E") {
+            alert("Introduzca un número de boleta valido");
+            return false;
+        }
     }
 
 
